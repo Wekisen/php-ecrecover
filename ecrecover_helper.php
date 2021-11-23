5e784175fd241c110f9acde3ec0e2b4e33ec3142
Wekisen-Comsand <i@wekisen.com>
2021-11-23 23:20:47 +0800

    fix

--- ecrecover_helper.php
+++ ecrecover_helper.php
@@ -5,22 +5,24 @@ require_once 'CryptoCurrencyPHP/Signature.class.php';
 // require_once './vendor/autoload.php';
 use kornrunner\Keccak;
 
-function personal_ecRecover($msg, $signed) {
-    $personal_prefix_msg = "\x19Ethereum Signed Message:\n". strlen($msg). $msg;
+function personal_ecRecover($msg, $signed)
+{
+    $personal_prefix_msg = "\x19Ethereum Signed Message:\n" . strlen($msg) . $msg;
     $hex = keccak256($personal_prefix_msg);
     return ecRecover($hex, $signed);
 }
 
-function ecRecover($hex, $signed) {
+function ecRecover($hex, $signed)
+{
     $rHex   = substr($signed, 2, 64);
     $sHex   = substr($signed, 66, 64);
     $vValue = hexdec(substr($signed, 130, 2));
     $messageHex       = substr($hex, 2);
     $messageByteArray = unpack('C*', hex2bin($messageHex));
     $messageGmp       = gmp_init("0x" . $messageHex);
-    $r = $rHex;		//hex string without 0x
-    $s = $sHex; 	//hex string without 0x
-    $v = $vValue; 	//27 or 28
+    $r = $rHex;        //hex string without 0x
+    $s = $sHex;     //hex string without 0x
+    $v = $vValue == 0 ? 27 : ($vValue == 1 ? 28 : $vValue); //27 or 28
 
     //with hex2bin it gives the same byte array as the javascript
     $rByteArray = unpack('C*', hex2bin($r));
@@ -36,7 +38,7 @@ function ecRecover($hex, $signed) {
     $publicKey = Signature::recoverPublicKey($rGmp, $sGmp, $messageGmp, $recovery);
     $publicKeyString = $publicKey["x"] . $publicKey["y"];
 
-    return '0x'. substr(keccak256(hex2bin($publicKeyString)), -40);
+    return '0x' . substr(keccak256(hex2bin($publicKeyString)), -40);
 }
 
 function strToHex($string)
@@ -45,6 +47,7 @@ function strToHex($string)
     return '0x' . array_shift($hex);
 }
 
-function keccak256($str) {
-    return '0x'. Keccak::hash($str, 256);
+function keccak256($str)
+{
+    return '0x' . Keccak::hash($str, 256);
 }
